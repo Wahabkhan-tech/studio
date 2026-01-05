37922324137fb7be33dc0b32427f9d6d91e7ae88
Abdul wahab <abdulwahabkhan321123@gmail.com>
2026-01-05 00:42:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3924069949-af8fc",
+  "appId": "1:260065536854:web:653508be27030596e404f7",
+  "apiKey": "AIzaSyAbdpJN8DBFy5K6U6Y5bUf9MKFg2Lu-8CA",
+  "authDomain": "studio-3924069949-af8fc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "260065536854"
+};
